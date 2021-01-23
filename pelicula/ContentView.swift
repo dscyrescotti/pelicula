c7d3de1ab228cadc727eab4ba7cbc1654dd89379
dscyrescotti <dscyrescotti@gmail.com>
2021-01-23 16:51:16 +0630

    feat: commit initially

---
+++ pelicula/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pelicula
+//
+//  Created by Dscyre Scotti on 23/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pelicula/peliculaApp.swift
@@ -0,0 +1,17 @@
+//
+//  peliculaApp.swift
+//  pelicula
+//
+//  Created by Dscyre Scotti on 23/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct peliculaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
